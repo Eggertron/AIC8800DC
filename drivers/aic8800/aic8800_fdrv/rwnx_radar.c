cb70956ffbcbc20500013da52f004bc2e1025711
Egger Haaaaan <bboyhan@hotmail.com>
2025-12-31 21:31:05 -0500

    Update rwnx_radar.c
    Ah, that clarifies it. Since you are on Kernel 6.12, you are dealing with the MLO (Multi-Link Operation) updates introduced for Wi-Fi 7. The function now requires a total of 5 arguments, including a link_id.
    
    The Fix
    To fix the error, you need to add both the chandef and the link_id to the function call. For most standard drivers not yet utilizing Multi-Link Operation, the link_id is typically set to 0.

--- drivers/aic8800/aic8800_fdrv/rwnx_radar.c
+++ drivers/aic8800/aic8800_fdrv/rwnx_radar.c
@@ -1399,7 +1399,9 @@ static void rwnx_radar_cac_work(struct work_struct *ws)
                     #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)
                        &ctxt->chan_def,
                     #endif
-                       NL80211_RADAR_CAC_FINISHED, GFP_KERNEL);
+                       NL80211_RADAR_CAC_FINISHED,
+                       GFP_KERNEL,
+                       0);  /* link_id: 0 for non-MLO / single link */
     rwnx_send_apm_stop_cac_req(rwnx_hw, radar->cac_vif);
     rwnx_chanctx_unlink(radar->cac_vif);
 
@@ -1499,7 +1501,9 @@ void rwnx_radar_cancel_cac(struct rwnx_radar *radar)
                         #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)
                            &ctxt->chan_def,
                         #endif
-                           NL80211_RADAR_CAC_ABORTED, GFP_KERNEL);
+                           NL80211_RADAR_CAC_ABORTED,
+                           GFP_KERNEL,
+                           0);  /* link_id: 0 for non-MLO / single link */
         rwnx_chanctx_unlink(radar->cac_vif);
     }
 
